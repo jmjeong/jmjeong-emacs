09be2b57ec50656f7d357655bd17eef3a70b35fe
Jaemok Jeong <jmjeong@gmail.com>
2009-11-09 16:01:56 +0900

    add alias for auto-install-from-emacswiki

--- jmjeong-autoinstall.el
+++ jmjeong-autoinstall.el
@@ -3,3 +3,5 @@
 (setq auto-install-directory (concat elisp-root-dir "/auto-install/"))
 ;(auto-install-update-emacswiki-package-name t)
 (setq auto-install-save-confirm nil)
+
+(defalias 'aiw 'auto-install-from-emacswiki)
