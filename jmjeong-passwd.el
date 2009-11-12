4f31b7f1e471d3b555b716d93012fbe780061f23
Jaemok Jeong <jmjeong@gmail.com>
2009-11-12 13:39:40 +0900

    change real-passwords to emacs-real-passwords and store this file to another repository

--- jmjeong-passwd.el
+++ jmjeong-passwd.el
@@ -19,4 +19,4 @@
 ;; You can also do as I do and keep them in a seperate folder:
 (add-to-list 'load-path "~/.emacs.private")
 ;;Load the password file but don't complain if it doesn't exist
-(load "real-passwords" 'noerror)
+(load "emacs-real-passwords" 'noerror)
