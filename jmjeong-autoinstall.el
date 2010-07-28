23381b951509943decc1b3d794eefcfb36112df5
jmjeong <jmjeong@jmjeong-ui-MacBook-Pro.local>
2010-07-28 12:10:48 +0900

    set url-http-attempt-keepalives variablee to t

--- jmjeong-autoinstall.el
+++ jmjeong-autoinstall.el
@@ -3,5 +3,6 @@
 (setq auto-install-directory (concat elisp-root-dir "/auto-install/"))
 ;(auto-install-update-emacswiki-package-name t)
 (setq auto-install-save-confirm nil)
+(setq url-http-attempt-keepalives t)
 
 (defalias 'aiw 'auto-install-from-emacswiki)
