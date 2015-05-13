c6113161c6b8e65d718292c89354f0f4be5a8545
Jaemok Jeong <jmjeong@gmail.com>
2015-05-13 19:04:25 +0900

    move package to elpa

--- jmjeong-etc.el
+++ jmjeong-etc.el
@@ -1,3 +1,15 @@
 (autoload 'rainbow-mode "rainbow-mode" nil t)
 (require 'js2-mode)
-;; (require 'coffee-mode)
+(require 'coffee-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(autoload 'tern-mode "tern.el" nil t)
+
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (setq tern-ac-on-dot t)
+     (tern-ac-setup)))
